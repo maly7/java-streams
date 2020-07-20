ad59b6445bb8be75ca9fc0b6fa9856ae8edab9b4
maly7 <paul.aly@outlook.com>
2020-07-20 18:51:02 -0400

    add MaxAndComparator solution

--- src/main/java/com/github/maly7/streams/MaxAndComparator.java
+++ src/main/java/com/github/maly7/streams/MaxAndComparator.java
@@ -1,16 +1,13 @@
 package com.github.maly7.streams;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class MaxAndComparator {
     public static Person getOldestPerson(List<Person> people) {
-        Person oldestPerson = Person.builder().name("").age(0).build();
-        for (Person person : people) {
-            if (person.getAge() > oldestPerson.getAge()) {
-                oldestPerson = person;
-            }
-        }
-        return oldestPerson;
+        return people.stream()
+                .max(Comparator.comparingInt(person -> person.getAge()))
+                .get();
     }
 
 }
