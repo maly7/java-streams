f4326a9d6cc61ffe0f20d34c22ce6d01d2db0858
maly7 <paul.aly@outlook.com>
2020-07-20 19:00:44 -0400

    revert solution

--- src/main/java/com/github/maly7/streams/PartitionBy.java
+++ src/main/java/com/github/maly7/streams/PartitionBy.java
@@ -1,13 +1,18 @@
 package com.github.maly7.streams;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static java.util.stream.Collectors.partitioningBy;
-
 public class PartitionBy {
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
-        return people.stream()
-                .collect(partitioningBy(person -> person.getAge() > 18));
+        Map<Boolean, List<Person>> map = new HashMap<>();
+        map.put(true, new ArrayList<>());
+        map.put(false, new ArrayList<>());
+        for (Person person : people) {
+            map.get(person.getAge() >= 18).add(person);
+        }
+        return map;
     }
 }
