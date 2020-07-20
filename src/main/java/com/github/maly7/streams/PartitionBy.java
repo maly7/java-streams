879bb7601e125c9509d384578931dffef1b6ca82
maly7 <paul.aly@outlook.com>
2020-07-20 19:01:21 -0400

    add PartitionBy solution

--- src/main/java/com/github/maly7/streams/PartitionBy.java
+++ src/main/java/com/github/maly7/streams/PartitionBy.java
@@ -5,14 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.partitioningBy;
+
 public class PartitionBy {
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
-        Map<Boolean, List<Person>> map = new HashMap<>();
-        map.put(true, new ArrayList<>());
-        map.put(false, new ArrayList<>());
-        for (Person person : people) {
-            map.get(person.getAge() >= 18).add(person);
-        }
-        return map;
+        return people.stream()
+                .collect(partitioningBy(person -> person.getAge() > 18));
     }
 }
