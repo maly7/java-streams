e2d4894963605ac0edf549fa1fe6d07d21dce890
maly7 <paul.aly@outlook.com>
2020-07-20 19:13:26 -0400

    add EvenOrOdd solution

--- src/main/java/com/github/maly7/streams/EvenOrOdd.java
+++ src/main/java/com/github/maly7/streams/EvenOrOdd.java
@@ -1,10 +1,13 @@
 package com.github.maly7.streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EvenOrOdd {
     public static String getString(List<Integer> list) {
-        return null;
+        return list.stream()
+                .map(value -> value % 2 == 0 ? "even " + value : "odd " + value)
+                .collect(Collectors.joining(", "));
     }
 
 }
