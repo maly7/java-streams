e8252d472165f2e46e14f94cc30be9566a886277
maly7 <paul.aly@outlook.com>
2020-07-20 18:54:07 -0400

    add SumAndReduce solution

--- src/main/java/com/github/maly7/streams/SumAndReduce.java
+++ src/main/java/com/github/maly7/streams/SumAndReduce.java
@@ -4,10 +4,7 @@ import java.util.List;
 
 public class SumAndReduce {
     public static int calculate(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return numbers.stream()
+                .reduce(0, (subtotal, value) -> subtotal + value);
     }
 }
