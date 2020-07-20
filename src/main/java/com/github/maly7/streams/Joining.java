f5d92c9acf9bbcc97befecde524f2c486a6be99a
maly7 <paul.aly@outlook.com>
2020-07-20 19:09:02 -0400

    add Joining solution

--- src/main/java/com/github/maly7/streams/Joining.java
+++ src/main/java/com/github/maly7/streams/Joining.java
@@ -1,18 +1,13 @@
 package com.github.maly7.streams;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Joining {
     public static String namesToString(List<Person> people) {
-        String label = "Names: ";
-        StringBuilder sb = new StringBuilder(label);
-        for (Person person : people) {
-            if (sb.length() > label.length()) {
-                sb.append(", ");
-            }
-            sb.append(person.getName());
-        }
-        sb.append(".");
-        return sb.toString();
+        var names = people.stream()
+                .map(person -> person.getName())
+                .collect(Collectors.joining(", "));
+        return "Names: " + names + ".";
     }
 }
