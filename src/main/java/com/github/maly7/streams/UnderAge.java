3bb4150517e5d1d2d1dafa016c6b0c9bfcdb6c95
maly7 <paul.aly@outlook.com>
2020-07-20 18:55:55 -0400

    add UnderAge solution

--- src/main/java/com/github/maly7/streams/UnderAge.java
+++ src/main/java/com/github/maly7/streams/UnderAge.java
@@ -1,17 +1,14 @@
 package com.github.maly7.streams;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class UnderAge {
     public static Set<String> getKidNames(List<Person> people) {
-        Set<String> kids = new HashSet<>();
-        for (Person person : people) {
-            if (person.getAge() < 18) {
-                kids.add(person.getName());
-            }
-        }
-        return kids;
+        return people.stream()
+                .filter(person -> person.getAge() < 18)
+                .map(person -> person.getName())
+                .collect(Collectors.toSet());
     }
 }
