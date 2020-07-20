5f1e654d3229e66fbbf2f745cbfd69a3f85b1f20
maly7 <paul.aly@outlook.com>
2020-07-20 18:39:34 -0400

    add letter count solution

--- src/main/java/com/github/maly7/streams/LetterCount.java
+++ src/main/java/com/github/maly7/streams/LetterCount.java
@@ -1,7 +1,12 @@
 package com.github.maly7.streams;
 
+import java.util.Arrays;
+
 public class LetterCount {
     public static int getTotalNumberOfLettersOfNamesLongerThanFive(String... names) {
-        return 0;
+        return Arrays.stream(names)
+                .filter(name -> name.length() > 5)
+                .mapToInt(name -> name.length())
+                .sum();
     }
 }
